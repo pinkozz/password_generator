fd27364d0ec7f9bf8caa774ebd77d0da61e55f39
pink_ozzy <136079534+pinkozz@users.noreply.github.com>
2023-06-12 00:00:05 +0300

    password_generator.py
    /comments added
    /changes in code

--- password_generator.py
+++ password_generator.py
@@ -1,13 +1,16 @@
 import secrets
 import string
 
+#Ask about the length of the password
 ask = input('How long do you want your password to be?: ')
 
+#Create a variable that contains all numbers and letters
 letters_digits = string.ascii_letters + string.digits
+
 password = ''
 
-for i in range(int(ask)):
-    digit = secrets.choice(letters_digits)
-    password += digit
+for i in range(int(ask)):                           #Repeat the process a certain amount of times
+    character = secrets.choice(letters_digits)      #Generate one character
+    password += digit                               #Add the digit to the password
 
 print(password)
